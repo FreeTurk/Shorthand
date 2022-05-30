8894a04433a3a0a273a5246466b9b6009adee79d
FreeTurk <teknolojigamers@gmail.com>
2022-05-30 12:29:13 +0000

    stuff

--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
+
+func errchk(i error) {
+	if i != nil {
+		log.Fatal(i)
+	}
+}
 
 func main() {
 	file, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
+	errchk(err)
+	scanner := bufio.NewScanner(file)
+	newfilename := strings.TrimSuffix(os.Args[1], ".soh") + ".go"
+	newfile, err := os.Create(newfilename)
+	_ = newfile
+	errchk(err)
+	newfile.WriteString("package main\n")
+
+	for scanner.Scan() {
+		parts1 := strings.SplitN(scanner.Text(), " ", 2)
+		if parts1[0] == "P" {
+		}
 	}
-	fmt.Println(file)
+
+	for scanner.Scan() {
+		parts1 := strings.SplitN(scanner.Text(), " ", 2)
+		fmt.Println(parts1[0])
+	}
 }
